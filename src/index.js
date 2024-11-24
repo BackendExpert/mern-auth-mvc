d3b799a274f98579fe2b8e61be3abfd798dcc109
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-11-24 11:55:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,5 +15,53 @@ const SignUpdestination = argv.destination || path.join(process.cwd(), 'src/page
 const Componentsource = argv.source || path.join(__dirname, '../temps/components');
 const Componentdestination = argv.destination || path.join(process.cwd(), 'src/components');
 
-const Componentsource = argv.source || path.join(__dirname, '../temps/components');
-const Componentdestination = argv.destination || path.join(process.cwd(), 'src/components');
+const Assestssource = argv.source || path.join(__dirname, '../temps/assets');
+const Assestsdestination = argv.destination || path.join(process.cwd(), 'src/assets');
+
+
+async function CreateDefaultFrontEndLoginSignUp (){
+    try {
+        await fs.copy(Loginsource, Logindestination);
+        await fs.copy(SignUpsource, SignUpdestination);
+        await fs.copy(Componentsource, Componentdestination);
+        await fs.copy(Assestssource, Assestsdestination);
+        console.log('Default Login and SignUp Templates Created Successful..');
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+
+async function main(){
+    try {
+        const answers = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'selection1',
+                message: 'Select Frontend Templates for Login and SignUp :',
+                choices: ['Default Template', 'will in Future Update'],
+            },
+        ]);
+      
+        const { selection1 } = answers;
+
+        switch (selection1) {
+            case 'Default Template':
+                await CreateDefaultFrontEndLoginSignUp();
+                break;
+    
+            case 'will in Future Update':
+                await CreateDefaultFrontEndLoginSignUp();       
+                break;
+    
+            default:
+              console.log('Invalid selection.');
+        }
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+main()
